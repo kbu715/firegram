a5976d0f923dbb24acdba5045a545fe7ae9f4fa3
unknown <kbu715@gmail.com>
2020-11-17 17:44:17 +0900

    readme

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -4,9 +4,9 @@ import { motion } from 'framer-motion';
 const Title = () => {
   return (
     <motion.div className="title" initial={{opacity: 0}} animate={{opacity: 1}}>
-      <h1>FireGram</h1>
-      <h2>Lovely Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1>파이어그램</h1>
+      <h2>러블리 픽쳐스</h2>
+      <p>러블리한 사진을 올려주세요</p>
     </motion.div>
   )
 }
