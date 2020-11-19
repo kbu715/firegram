ddfd02f581f2d25a31081d8c58fb8713e41adb73
kbu715 <kbu715@gmail.com>
2020-11-19 18:58:40 +0900

    Updates

---
+++ precache-manifest.a7be46c455d2844e2acf7517c1791bc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "450fc09e171b22ad38327df245e610f3",
+    "url": "/firegram/index.html"
+  },
+  {
+    "revision": "4a48f828b8d145caa289",
+    "url": "/firegram/static/css/main.c61ef27f.chunk.css"
+  },
+  {
+    "revision": "3338cfb596ff63adbf8c",
+    "url": "/firegram/static/js/2.18280ed8.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/firegram/static/js/2.18280ed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a48f828b8d145caa289",
+    "url": "/firegram/static/js/main.3d26be22.chunk.js"
+  },
+  {
+    "revision": "a20b20e63cdcd0ca9e81",
+    "url": "/firegram/static/js/runtime-main.9968160c.js"
+  }
+]);
